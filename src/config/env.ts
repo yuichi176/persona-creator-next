5e2f1e72eacdcf98fa74c52194b1c1a89cccb3a4
Yuichi Sugiyama <ozwald176@gmail.com>
2024-07-09 15:53:18 +0900

    Fix env name

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,6 +3,5 @@ type Env = {
 }
 
 export const env: Env = {
-  PERSONA_CREATOR_API_BASE_URL:
-    process.env.VITE_PERSONA_CREATOR_API_BASE_URL ?? 'http://localhost:8080',
+  PERSONA_CREATOR_API_BASE_URL: process.env.PERSONA_CREATOR_API_BASE_URL ?? 'http://localhost:8080',
 }
